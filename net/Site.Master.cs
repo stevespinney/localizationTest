fd9de4ed214839c1b22f07083eafb9d4b2a96823
Steve Spinney <sspinney@gelia.com>
2012-08-02 12:24:31 -0400

    On branch master  Changes to be committed: 	modified:   net/Site.Master.cs

--- net/Site.Master.cs
+++ net/Site.Master.cs
@@ -17,38 +17,31 @@ namespace net
 		protected void engCulture_Click(object sender, EventArgs e)
         {
 			Session["culture"] = "en-US";
-			String _page = Request.Url.LocalPath;
-			_page = "~" + _page;
+            String _page = Request.Url.AbsoluteUri;
 			Response.Redirect(_page);
         }
 		protected void spanCulture_Click(object sender, EventArgs e)
         {
 			Session["culture"] = "es-MX";
-            //String _page = Request.Url.LocalPath;
             String _page = Request.Url.AbsoluteUri;
-            //String server = Request.Url.Host;
-			//_page = _page;
 			Response.Redirect(_page);
         }
 		protected void rusCulture_Click(object sender, EventArgs e)
         {
 			Session["culture"] = "ru-RU";
-			String _page = Request.Url.LocalPath;
-			_page = "~" + _page;
+            String _page = Request.Url.AbsoluteUri;
 			Response.Redirect(_page);
         }
 		protected void gerCulture_Click(object sender, EventArgs e)
         {
 			Session["culture"] = "de-DE";
-			String _page = Request.Url.LocalPath;
-			_page = "~" + _page;
+            String _page = Request.Url.AbsoluteUri;
 			Response.Redirect(_page);
         }
 		protected void egyCulture_Click(object sender, EventArgs e)
         {
 			Session["culture"] = "ar-EG";
-			String _page = Request.Url.LocalPath;
-			_page = "~" + _page;
+            String _page = Request.Url.AbsoluteUri;
 			Response.Redirect(_page);
         }
     }
